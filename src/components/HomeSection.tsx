6994a8ada5c59f4cdab6eb07a7752ce248032359
Mateus Abreu <mateus.nascimento@unirv.edu.br>
2025-03-11 09:37:42 -0300

    teste view heigth

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -11,14 +11,14 @@ export default function HomeSection() {
         Agende sua consulta e comece a mudança hoje
       </div>
 
-      <div className="relative h-[calc(100dvh-240px)] w-full bg-primary rounded-lg mt-4 z-40 transition-all duration-300 hover:scale-105 hover:mt-6">
+      <div className="relative h-[calc(100vh-240px)] w-full bg-primary rounded-lg mt-4 z-40 transition-all duration-300 hover:scale-105 hover:mt-6">
         <Badge className="absolute top-4 left-4 z-50 text-secondary bg-softWhite text-sm sm:text-base md:text-lg hover:bg-softWhite">
           Atendimento Presencial ou Online
         </Badge>
 
         <div className="absolute top-20 left-4 z-50 flex flex-col gap-4 text-xl md:text-2xl text-softWhite font-bold">
           <div className="relative w-fit">
-            <div className="md:flex hidden">
+            <div className="sm:flex hidden">
               <div className="absolute left-0 bottom-0 w-full h-1/2 bg-primary z-30 blur-sm"></div>
 
               <div className="relative z-40">
@@ -26,11 +26,13 @@ export default function HomeSection() {
               </div>
             </div>
 
-            <div className="md:hidden">
+            <div className="sm:hidden">
               <div className="absolute left-0 bottom-0 w-[118px] h-1/4 bg-primary z-30 blur-sm"></div>
               <div className="absolute left-0 bottom-7 w-full h-1/4 bg-primary z-30 blur-sm"></div>
 
-              <div className="relative z-40">Fisioterapeuta com 9 anos de</div>
+              <div className="relative z-40 whitespace-nowrap">
+                Fisioterapeuta com 9 anos de
+              </div>
 
               <div className="relative z-40">experiência</div>
             </div>
